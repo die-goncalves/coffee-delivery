f43cedff59ff1105d4deb857a097fef3f7120b49
die-goncalves <die.goncalves1990@gmail.com>
2022-08-19 16:31:11 -0300

    Adicionar scrollbar customizado

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
-import { LayoutContainer } from './styles'
+import { LayoutContainer, MainContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <LayoutContainer>
-      <Header />
-      <Outlet />
-    </LayoutContainer>
+    <MainContainer>
+      <LayoutContainer>
+        <Header />
+        <Outlet />
+      </LayoutContainer>
+    </MainContainer>
   )
 }
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components'
 
+export const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  scrollbar-gutter: stable;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 0.8vw;
+    height: 0.8vw;
+    background: ${props => props.theme['base/background']};
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${props => props.theme['brand/purple-light']};
+    &:hover {
+      background: ${props => props.theme['brand/purple']};
+    }
+    &:active {
+      background: ${props => props.theme['brand/purple-dark']};
+    }
+  }
+  &::-webkit-scrollbar-thumb {
+  }
+`
+
 export const LayoutContainer = styled.div`
   max-width: 1440px;
   margin: 0 auto;
