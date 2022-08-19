efa248941405457da025491b08b121c0d69337b2
die-goncalves <die.goncalves1990@gmail.com>
2022-08-19 16:19:07 -0300

    Mostrar quantidade de cafés adicionados no carrinho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,45 @@
 import CoffeeDeliveryLogo from '../../assets/coffee-delivery.svg'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 import {
-  CartLink,
-  CoffeeDeliveryLogoLink,
+  CartContainer,
   HeaderContainer,
-  LocationAndCartContainer,
-  LocationLink
+  LocationAndCartContainer
 } from './styles'
+import { useCart } from '../../hooks/useCart'
+import { Link } from 'react-router-dom'
 
 export function Header() {
+  const { cart } = useCart()
+
+  const quantityCoffee = cart.reduce((acc, currentItem) => {
+    return acc + currentItem.quantity
+  }, 0)
+
   return (
     <HeaderContainer>
-      <CoffeeDeliveryLogoLink href="/">
+      <Link to="/">
         <img src={CoffeeDeliveryLogo} alt="Entrega de café" />
-      </CoffeeDeliveryLogoLink>
+      </Link>
 
       <LocationAndCartContainer>
-        <LocationLink href="#">
+        <Link to="geolocation">
           <MapPin weight="fill" />
           &nbsp;<span>Porto Alegre, RS</span>
-        </LocationLink>
-        <CartLink href="#">
-          <ShoppingCart weight="fill" />
-        </CartLink>
+        </Link>
+
+        <CartContainer>
+          <div>
+            {quantityCoffee > 99 && <span>+</span>}
+            {quantityCoffee > 99 ? (
+              <span>99</span>
+            ) : (
+              <span>{quantityCoffee}</span>
+            )}
+          </div>
+          <Link to="/checkout">
+            <ShoppingCart weight="fill" />
+          </Link>
+        </CartContainer>
       </LocationAndCartContainer>
     </HeaderContainer>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,50 +2,80 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.nav`
   display: flex;
+  align-items: center;
   position: sticky;
   z-index: 99999;
   top: 0;
   justify-content: space-between;
   padding: 2rem 10rem;
   background: ${props => props.theme['base/background']};
-`
 
-export const CoffeeDeliveryLogoLink = styled.a`
-  font-size: 0;
+  & > a {
+    font-size: 0;
+  }
 `
 
 export const LocationAndCartContainer = styled.div`
   display: flex;
   gap: 0.75rem;
-`
 
-export const LocationLink = styled.a`
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  border-radius: 6px;
-  text-decoration: none;
-  color: ${props => props.theme['brand/purple-dark']};
-  background: ${props => props.theme['brand/purple-light']};
+  & > a {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    border-radius: 6px;
+    text-decoration: none;
+    color: ${props => props.theme['brand/purple-dark']};
+    background: ${props => props.theme['brand/purple-light']};
 
-  svg {
-    flex: none;
-    font-size: 1.375rem;
-    color: ${props => props.theme['brand/purple']};
+    svg {
+      flex: none;
+      font-size: 1.375rem;
+      color: ${props => props.theme['brand/purple']};
+    }
   }
 `
 
-export const CartLink = styled.a`
+export const CartContainer = styled.div`
   display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  border-radius: 6px;
-  text-decoration: none;
-  background: ${props => props.theme['brand/yellow-light']};
+  position: relative;
+  margin: auto 0;
 
-  svg {
-    flex: none;
-    font-size: 1.375rem;
-    color: ${props => props.theme['brand/yellow-dark']};
+  & > a {
+    padding: 0.5rem;
+    border-radius: 6px;
+    text-decoration: none;
+    font-size: 0;
+    background: ${props => props.theme['brand/yellow-light']};
+
+    svg {
+      flex: none;
+      font-size: 1.375rem;
+      color: ${props => props.theme['brand/yellow-dark']};
+    }
+  }
+
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 99999px;
+    top: 0;
+    right: 0;
+    transform: translate(50%, -50%);
+    background: ${props => props.theme['brand/yellow-dark']};
+    color: ${props => props.theme['base/white']};
+
+    & > span:first-child {
+      font-size: 0.5rem;
+      font-weight: 400;
+    }
+    & > span:last-child {
+      font-size: 0.75rem;
+      font-weight: 700;
+    }
   }
 `
